76096b578c15c6b9d8136e744951d1886f2e99d1
aleff-eco <dev.aleffec@gmail.com>
2025-06-23 12:57:56 -0600

    Fix | seo otra vez

--- src/app/layout.js
+++ src/app/layout.js
@@ -82,10 +82,10 @@ export const metadata = {
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
+    apple: "/favicon.ico",
     other: [
-      { rel: "icon", url: "/favicon-32x32.png", sizes: "32x32" },
-      { rel: "icon", url: "/favicon-16x16.png", sizes: "16x16" },
+      { rel: "icon", url: "/favicon.ico", sizes: "32x32" },
+      { rel: "icon", url: "/favicon.ico", sizes: "16x16" },
     ],
   },
 };
